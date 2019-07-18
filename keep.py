f4dfa51ab80ee3e473857da0f2843c4fc812adcb
Kidonsky <bryan.pereira@protonmail.com>
2019-07-18 18:17:16 +0200

    [Tab] Change tabs in spaces.

--- keep.py
+++ keep.py
@@ -13,27 +13,27 @@ writer = csv.writer(open(csvout, 'wb'))
 writer.writerow(['file', 'title', 'content'])
 
 for file in files:
-	print(file)
-	page = open(file)
-	soup = bs4.BeautifulSoup(page.read(), "html.parser")
+    print(file)
+    page = open(file)
+    soup = bs4.BeautifulSoup(page.read(), "html.parser")
 
-	#Get title
-	if len(soup.select('.title')) == 0:
-		title = ''
-	else:
-		title = soup.select('.title')[0].getText()
+    #Get title
+    if len(soup.select('.title')) == 0:
+        title = ''
+    else:
+        title = soup.select('.title')[0].getText()
 
-	#Parse Content
-	html = soup.select(".content")[0]
-	#Convert linebreaks
-	for br in soup.find_all("br"):
-		br.replace_with("\n")
-	content = html.getText()
-	
-	note = {
-		"title": title,
-		"content": content
-	}
-	writer.writerow([file,note['title'],note['content']])
+    #Parse Content
+    html = soup.select(".content")[0]
+    #Convert linebreaks
+    for br in soup.find_all("br"):
+        br.replace_with("\n")
+    content = html.getText()
+    
+    note = {
+        "title": title,
+        "content": content
+    }
+    writer.writerow([file,note['title'],note['content']])
 
 print('\n'+'-'*50 + '\nDone! %s notes saved to %s\n' % (len(files), csvout))
